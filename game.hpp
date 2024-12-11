f4b4f9c1d54f610a0faaecd6b0ec765cb38504a5
Trefla25 <11alexandru12@gmail.com>
2024-12-11 15:45:34 +0000

    c

--- game.hpp
+++ game.hpp
@@ -1,39 +1,37 @@
-#include "media.hpp"
-using namespace std;
+                                                    #include "media.hpp"
 
+                                                    class Game : public Media {
+                                                    private:
+                                                        double price;
+                                                        double rating;
 
-class Game : public Media {
-private:
-    double price;
-    double rating;
+                                                    public:
+                                                        // Default 
+                                                        Game() : Media() {
+                                                            price = 0.0;
+                                                            rating = 0.0;
+                                                            std::cout << "Derived Game object created with default constructor: " << title << std::endl;
+                                                        }
 
-public:
-    // Default 
-    Game() : Media() {
-        price = 0.0;
-        rating = 0.0;
-        std::cout << "Derived Game object created with default constructor: " << title << std::endl;
-    }
+                                                        // Normal 
+                                                        Game(const char* init_title, const char* init_developer, double init_price, double init_rating)
+                                                            : Media(init_title, init_developer), price(init_price), rating(init_rating) {
+                                                            std::cout << "Derived Game object created with normal constructor: " << title << std::endl;
+                                                        }
+                                                        //Deconstructor
+                                                        ~Game() override {
+                                                            std::cout << "Derived Game object destroyed: " << title << std::endl;
+                                                        }
 
-    // Normal 
-    Game(const char* init_title, const char* init_developer, double init_price, double init_rating)
-        : Media(init_title, init_developer), price(init_price), rating(init_rating) {
-        std::cout << "Derived Game object created with normal constructor: " << title << std::endl;
-    }
-    //Deconstructor
-    ~Game() override {
-        std::cout << "Derived Game object destroyed: " << title << std::endl;
-    }
+                                                        //Display
+                                                        void displayInfo() const override {
+                                                            Media::displayInfo();
+                                                            std::cout << "Price: " << price << ", Rating: " << rating << std::endl;
+                                                        }
 
-    //Display
-    void displayInfo() const override {
-        Media::displayInfo();
-        std::cout << "Price: " << price << ", Rating: " << rating << std::endl;
-    }
-
-    // Disablimg copy and move constructors and assignment operators
-    Game(const Game&) = delete;
-    Game& operator=(const Game&) = delete;
-    Game(Game&&) = delete;
-    Game& operator=(Game&&) = delete;
-};
+                                                        // Disablimg copy and move constructors and assignment operators
+                                                    //Game(const Game&) = delete;
+                                                        //Game& operator=(const Game&) = delete;
+                                                        //Game(Game&&) = delete;
+                                                        //Game& operator=(Game&&) = delete;
+                                                    };
